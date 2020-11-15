12ed77185eeb532d91e40fe538b174196464ecf5
audiofrequenz <audiofrequenz@gmail.com>
2020-11-15 16:28:04 +0100

    added comments to location and coordinate code

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -14,12 +14,16 @@ public class Coordinate {
     private double x;
     private double y;
     private double z;
+    //not part of homework cw2 but nice addon
     private CoordinateType type = CoordinateType.UNDEFINED;
 
     /**
-     *
+     * Coordinate constructor implements a new object with given features
+     * @param x x-value of Coordinate
+     * @param y y-value of Coordinate
+     * @param z z-value of Coordinate
+     * @param type needs to be a CoordinateType Object
      */
-
     public Coordinate(double x, double y, double z, CoordinateType type) {
         this.x = x;
         this.y = y;
@@ -27,10 +31,20 @@ public class Coordinate {
         this.type = type;
     }
 
+    /**
+     * Calculates the distance between two cartesian coordinates
+     * @param coordinate type Coordinate
+     * @return double with cartesian distance between current coordinate and given parameter coordinate
+     */
     public double getDistance(Coordinate coordinate){
         return Math.sqrt(Math.pow(coordinate.x - this.x, 2) + Math.pow(coordinate.y - this.y, 2) + Math.pow(coordinate.z - this.z, 2));
     }
 
+    /**
+     * Override for equals to check if given object equals current object
+     * @param obj type Object
+     * @return true if object equals current instance
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -41,38 +55,75 @@ public class Coordinate {
         return isEqual(other);
     }
 
+    /**
+     * checks whether the x, y and z coordinates of a given object is equal to the current object
+     * @param other type Coordinate
+     * @return true if x, y, z and type value of a coordinate equals the x, y, z, type coordinate of the current object
+     */
     public boolean isEqual(Coordinate other){
-        return other.x == this.x && other.y == this.y && other.z == this.z;
+        return other.x == this.x && other.y == this.y && other.z == this.z && other.type == this.type;
     }
 
+    /**
+     * Getter for x value
+     * @return x
+     */
     public double getX() {
         return x;
     }
 
+    /**
+     * Setter for x value
+     * @param x type double
+     */
     public void setX(double x) {
         this.x = x;
     }
 
+    /**
+     * Getter for y value
+     * @return y
+     */
     public double getY() {
         return y;
     }
 
+    /**
+     * Setter for y value
+     * @param y
+     */
     public void setY(double y) {
         this.y = y;
     }
 
+    /**
+     * Getter for z value
+     * @return z
+     */
     public double getZ() {
         return z;
     }
 
+    /**
+     * Setter for z value
+     * @param z
+     */
     public void setZ(double z) {
         this.z = z;
     }
 
+    /**
+     * Getter for type value
+     * @return type
+     */
     public CoordinateType getType() {
         return type;
     }
 
+    /**
+     * Setter for type value
+     * @param type
+     */
     public void setType(CoordinateType type) {
         this.type = type;
     }
--- src/main/java/org/wahlzeit/model/Location.java
+++ src/main/java/org/wahlzeit/model/Location.java
@@ -3,6 +3,10 @@ package org.wahlzeit.model;
 public class Location {
     protected Coordinate coordinate;
 
+    /**
+     * Location constructor implements a new object with given features
+     * @param coordinate type Coordinate
+     */
     public Location(Coordinate coordinate){
         this.coordinate = coordinate;
     }
